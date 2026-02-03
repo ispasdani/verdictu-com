a13df8fd79be1c4c9869791a3b38ac1bc3d35e08
ispasdani <ispasdani14@gmail.com>
2026-02-03 22:29:03 +0100

    diagonal pattern introduced

--- components/marketing-sections/hero.tsx
+++ components/marketing-sections/hero.tsx
@@ -1,10 +1,13 @@
 import BorderedSection from "../marketing-general/bordered-section";
+import DiagonalPattern from "../marketing-general/diagonal-pattern";
 
 function Hero() {
   return (
     <section className="pb-16 text-center lg:pb-0">
       <div className="flex">
-        <BorderedSection>test</BorderedSection>
+        <BorderedSection>
+          <DiagonalPattern />
+        </BorderedSection>
       </div>
     </section>
   );
