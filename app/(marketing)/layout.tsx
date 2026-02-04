73ef8d7ca291b926818ca8e651757739b793e141
ispasdani <ispasdani14@gmail.com>
2026-02-04 22:21:23 +0100

    navbar introduced

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,14 @@
+import Navbar from "@/components/marketing-general/navbar";
+
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
